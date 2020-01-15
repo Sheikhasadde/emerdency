e55a3e53b051b57f8248d3e761443f1aba8f80fc
asadscreations <sheikhasadde@gmail.com>
2020-01-15 15:57:09 -0800

    db connection updated

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,9 +1,13 @@
 <?php
 	function dbConnect(){
-		$dbServer = '10.169.0.169';
-  		$dbUsername = 'emerdenc_web';
-   		$dbPassword = '?n_qdqBc9C';
-   		$database= 'emerdenc_db';
+		// $dbServer = '10.169.0.169';
+  		// $dbUsername = 'emerdenc_web';
+   		// $dbPassword = '?n_qdqBc9C';
+		   // $database= 'emerdenc_db';
+		$dbServer = 'nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+  		$dbUsername = 's2hb541w4qgd4xhy';
+   		$dbPassword = 'i0nbrvslkcfjvcxk';
+   		$database= 'k6pxm0qh2379fwk3';
 		
    		$db = new mysqli($dbServer,$dbUsername, $dbPassword, $database);
     
